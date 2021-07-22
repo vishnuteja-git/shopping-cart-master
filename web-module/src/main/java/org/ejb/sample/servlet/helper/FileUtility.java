a1d682c85450454e9b562e821bd27dc8de095412
Rangarajan <rangarajan.ramadass@cognizant.com>
2021-07-22 20:51:01 +0530

    Fix for remediation :1-filesystemcustom-filesystemcustom-customrule

--- web-module/src/main/java/org/ejb/sample/servlet/helper/FileUtility.java
+++ web-module/src/main/java/org/ejb/sample/servlet/helper/FileUtility.java
@@ -2,17 +2,21 @@ package org.ejb.sample.servlet.helper;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import com.amazonaws.auth.*;
+import com.amazonaws.services.s3.*;
 
 public class FileUtility {
 
-        void generateReportFile(String content) throws IOException {
+    void generateReportFile(String content) throws IOException {
+        FileWriter fileWriter = new FileWriter("~/DailyReports/Report.csv");
+        fileWriter.write(content);
+        fileWriter.close();
+        uploadS3ObjectAndGetUrl("myBucket", "~/DailyReports/Report.csv", "Report.csv");
+    }
 
-            FileWriter fileWriter = new FileWriter("~/DailyReports/Report.csv");
-            fileWriter.write(content);
-            fileWriter.close();
-        }
-
+    public URL uploadS3ObjectAndGetUrl(String bucketName, String uploadContentPath, String keyName) {
+        AmazonS3 s3client = getS3Client();
+        s3client.putObject(new PutObjectRequest(bucketName, keyName, new File(uploadContentPath)));
+        return s3client.getUrl(bucketName, keyName);
+    }
 }
-
-
-
